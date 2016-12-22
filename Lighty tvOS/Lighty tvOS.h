cc44d011931256d587218c771d4a83c6570dafa6
abdullahselek <abdullahselek@gmail.com>
2016-12-22 08:32:17 +0300

    - add target for tvOS

---
+++ Lighty tvOS/Lighty tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Lighty tvOS.h
+//  Lighty tvOS
+//
+//  Created by Abdullah Selek on 22/12/2016.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lighty tvOS.
+FOUNDATION_EXPORT double Lighty_tvOSVersionNumber;
+
+//! Project version string for Lighty tvOS.
+FOUNDATION_EXPORT const unsigned char Lighty_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lighty_tvOS/PublicHeader.h>
+
+
