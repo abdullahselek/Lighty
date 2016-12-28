c01ee8dc0d16b62475c6809079dc9f99d22c35d5
abdullahselek <abdullahselek@gmail.com>
2016-12-28 10:31:28 +0300

    - update dlog

--- Sources/LightyLogger.swift
+++ Sources/LightyLogger.swift
@@ -135,7 +135,12 @@ public class LightyLogger {
                     function: String = #function,
                     line: Int = #line) {
         #if DEBUG
-            log(type: type, message: message)
+            let fileExtension = file.nsstring.lastPathComponent.nsstring.pathExtension
+            let fileName = file.nsstring.lastPathComponent.nsstring.deletingPathExtension
+
+            let trackedString = "\(fileName).\(fileExtension):\(line) \(function)"
+            let emoji = getAccessoryWithType(messageType: type)
+            print(emoji + " " + dateFormatter.string(from: Date()) + separator + trackedString + separator +  message + " " + emoji)
         #endif
     }
     
